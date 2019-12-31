a54d01b0b12742f9149b9fc37895a3780461cbbd
Jimmy CUI <38402619+jimmycgz@users.noreply.github.com>
2019-12-31 14:48:00 -0500

    Update kf-log-del.sh

--- kf-log-del.sh
+++ kf-log-del.sh
@@ -1,3 +1,10 @@
+df -h
+
+#Delete log files oldder than 5 days
 find /kafka/logs -name "*.log" -type f -mtime +5 -exec rm -f {} \;
+df -h
+
+#Change log retention as 72 hours
 echo "log.retention.hours=72" >> /opt/kafka/config/kafka.properties
-systemctl restart kafka
+sudo systemctl restart kafka #may experience time out here, add waiting time as needed.
+systemctl status kafka.service
